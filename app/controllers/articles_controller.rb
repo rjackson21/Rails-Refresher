951ccbfb30334d29a31b0189029b55b986b2f202
Regina Jackson <rjackson156@gmail.com>
2023-03-04 13:36:10 -0500

    using strong parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,11 +14,16 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(title: "...", body: "...")
 
-    if article save
+    if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 
 end
